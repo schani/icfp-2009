fcf960f5b228809f13e8b42e7109d665746317f4
Josef Widder <widder@ecs.tuwien.ac.at>
2009-06-28 00:00:07 +0200

    distance mit korrektuer und drumm und drann.

---
+++ ellipse_josef/newnewdistance.ml
@@ -0,0 +1,35 @@
+let ensure_les_than_pi a =
+  if a > pi then
+    a -. (2.0 *. pi)
+  else
+    a;;
+
+let ensure_greater_than_minus_pi a =
+  if a < (inve pi) then
+    a +. (2.0 *. pi)
+  else
+    a;;
+
+let differenz ankunft sie uhrzeiger =
+  let normalisierte_winkel =
+      (ensure_les_than_pi (pi -. sie +. ankunft))
+  in
+  if uhrzeiger then
+    (inve pi) -. normalisierte_winkel  
+  else
+    pi -. normalisierte_winkel    
+;;
+
+
+(* let differenz ankunft sie uhrzeiger = *)
+(*   let normalisierte_winkel = *)
+(*     if sie > 0.0 then *)
+(*       (ensure_les_than_pi (pi -. sie +. ankunft)) *)
+(*     else *)
+(*       (ensure_greater_than_minus_pi (ankunft -. (pi +. sie))) *)
+(*   in *)
+(*   if uhrzeiger then *)
+(*     (inve pi) -. normalisierte_winkel   *)
+(*   else *)
+(*     pi -. normalisierte_winkel     *)
+(* ;; *)
