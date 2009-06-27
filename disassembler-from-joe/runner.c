f205edbf2b5a17448469d792f16aecc48907d435
Mark Probst <mark.probst@gmail.com>
2009-06-27 13:12:11 +0200

    Disassembler: Better simulator decomposition for C generator.

--- disassembler-from-joe/runner.c
+++ disassembler-from-joe/runner.c
@@ -1,15 +1,19 @@
 #include <math.h>
 #include <stdio.h>
 
-static void
-simulate (void)
-{
 #include "out.c"
-}
 
 int
 main (void)
 {
-    simulate();
+    int iter;
+    machine_state_t state;
+    init_machine(&state);
+    state.input_16000 = 1001.0;
+    iter = 0;
+    while (++iter < 3000000 && state.output[0] == 0.0) {
+	//printf("%f    %f     %f\n", state.output[2], state.output[3], state.output[4]);
+	timestep(&state);
+    }
     return 0;
 }
