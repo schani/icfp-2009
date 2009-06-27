4cd1c6b15c85b1b1dfbbaf953aed0e753edf14e5
Mark Probst <mark.probst@gmail.com>
2009-06-27 02:07:23 +0200

    Disassembler: Simplify code in decompilation by building uses.

--- disassembler-from-joe/disassembler.py
+++ disassembler-from-joe/disassembler.py
@@ -123,18 +123,80 @@ class CodeCollector (CodeSink):
 		self.mem.append(mem)
 		self.code.append(('input', r1))
 
+def op_args(op):
+	if op[0] == 'alu':
+		return [op[2], op[3]]
+	elif op[0] == 'output' or op[0] == 'cmpz':
+		return [op[2]]
+	elif op[0] == 'phi':
+		return [op[1], op[2]]
+	elif op[0] == 'noop' or op[0] == 'input':
+		return []
+	elif op[0] == 'sqrt' or op[0] == 'copy':
+		return [op[1]]
+	else:
+		raise Exception('Unknown opcode %s' % op[0])
+
+def var_name(addr):
+	return 'v%s' % addr
+
 class Decompiler (CodeCollector):
 	def __init__(self):
 		CodeCollector.__init__(self)
 
-	def var_or_const(self,addr):
+	def build_uses(self):
+		self.uses = [[] for i in range(len(self.code))]
+		for addr in range(len(self.code)):
+			op = self.code[addr]
+			args = op_args(op)
+			for arg in args:
+				self.uses[arg].append(addr)
+
+	def var_or_const(self, addr, need_parens = False):
 		op = self.code[addr]
 		if op[0] == 'noop':
 			return self.mem[addr]
 		elif op[0] == 'cmpz' or op[0] == 'output':
 			raise Exception('Schweinerei')
+		elif not self.need_print_insn(addr):
+			rhs = self.op_rhs(addr)
+			if need_parens:
+				return '(%s)' % rhs
+			else:
+				return rhs
 		else:
-			return 'v%d' % addr
+			return var_name(addr)
+
+	def op_rhs(self, addr):
+		op = self.code[addr]
+		if op[0] == 'alu':
+			return '%s %s %s' % \
+			      (self.var_or_const(op[2], True), op[1], self.var_or_const(op[3], True))
+		elif op[0] == 'output':
+			return '%s' % self.var_or_const(op[2])
+		elif op[0] == 'sqrt':
+			return 'sqrt %s' % self.var_or_const(op[1], True)
+		elif op[0] == 'copy':
+			return self.var_or_const(op[1])
+		elif op[0] == 'input':
+			return 'port[%s]' % op[1]
+		elif op[0] == 'phi':
+			return var_name(addr)
+		elif op[0] == 'cmpz' or op[0] == 'noop':
+			raise Exception('Schweinerei')
+		else:
+			raise Exception('Unknown opcode %s' % op[0])
+
+	def need_print_insn(self, addr):
+		op = self.code[addr]
+		if op[0] == 'output' or op[0] == 'cmpz' or op[0] == 'phi':
+			return True
+		elif len(self.uses[addr]) == 0:
+			return False
+		elif len(self.uses[addr]) == 1:
+			return self.uses[addr][0] <= addr
+		else:
+			return True
 
 	def decompile(self):
 		#print 'have %d insns' % len(self.code)
@@ -142,12 +204,12 @@ class Decompiler (CodeCollector):
 		while addr < len(self.code):
 			op = self.code[addr]
 			#print 'decompiling insn %d: %s' % (addr, op)
-			if op[0] == 'alu':
-				print '%s = %s %s %s' % \
-				      (self.var_or_const(addr), self.var_or_const(op[2]), \
-				       op[1], self.var_or_const(op[3]))
+			if not self.need_print_insn(addr):
+				pass
+			elif op[0] == 'alu':
+				print '%s = %s' % (var_name(addr), self.op_rhs(addr))
 			elif op[0] == 'output':
-				print 'output %s' % self.var_or_const(op[1])
+				print 'port[%s] = %s' % (op[1], self.op_rhs(addr))
 			elif op[0] == 'phi':
 				raise Exception('Schweinerei')
 			elif op[0] == 'noop':
@@ -156,22 +218,23 @@ class Decompiler (CodeCollector):
 				phi = self.code[addr+1]
 				if phi[0] != 'phi':
 					raise Exception('Schweinerei')
-				print 'if %s %s 0:' % (self.var_or_const(op[2]), op[1])
-				print '    %s = %s' % (self.var_or_const(addr+1), self.var_or_const(phi[1]))
+				print 'if %s %s 0:' % (self.var_or_const(op[2], True), op[1])
+				print '    %s = %s' % (var_name(addr+1), self.var_or_const(phi[1]))
 				print 'else:'
-				print '    %s = %s' % (self.var_or_const(addr+1), self.var_or_const(phi[2]))
+				print '    %s = %s' % (var_name(addr+1), self.var_or_const(phi[2]))
 				addr += 1
 			elif op[0] == 'sqrt':
-				print '%s = sqrt %s' % (self.var_or_const(addr), self.var_or_const(op[1]))
+				print '%s = %s' % (var_name(addr), self.op_rhs(addr))
 			elif op[0] == 'copy':
-				print '%s = %s' % (self.var_or_const(addr), self.var_or_const(op[1]))
+				print '%s = %s' % (var_name(addr), self.op_rhs(addr))
 			elif op[0] == 'input':
-				print '%s = port[%s]' % (self.var_or_const(addr), op[1])
+				print '%s = %s' % (var_name(addr), self.op_rhs(addr))
 			else:
 				raise Exception('Unknown opcode %s' % op[0])
 			addr += 1
 
 	def finish(self):
+		self.build_uses()
 		self.decompile()
 
 if len(sys.argv) > 1 and sys.argv[1] == "-d":
@@ -220,15 +283,15 @@ while 1:
 			backend.noop_op(val)
 		elif op2 == 1:
 			if cmpop == 0:
-				cmpop = "LTZ"
+				cmpop = "ltz"
 			elif cmpop == 1:
-				cmpop = "LEZ"
+				cmpop = "lez"
 			elif cmpop == 2:
-				cmpop = "EQZ"
+				cmpop = "eqz"
 			elif cmpop == 3:
-				cmpop = "GEZ"
+				cmpop = "gez"
 			elif cmpop == 4:
-				cmpop = "GTZ"
+				cmpop = "gtz"
 			else:
 				raise Exception("Invalid immediate (%d) in Cmpz" % op)
 			backend.cmpz_op(val, cmpop, r1)
@@ -245,16 +308,16 @@ while 1:
 		r2 = (i & 0x3fff)
 
 		if op1 == 1:
-			backend.alu_op(val, "Add", r1, r2)
+			backend.alu_op(val, "add", r1, r2)
 		elif op1 == 2:
-			backend.alu_op(val, "Sub", r1, r2)
+			backend.alu_op(val, "sub", r1, r2)
 		elif op1 == 3:
-			backend.alu_op(val, "Mult", r1, r2)
+			backend.alu_op(val, "mult", r1, r2)
 		elif op1 == 4:
-			backend.alu_op(val, "Div", r1, r2)
+			backend.alu_op(val, "div", r1, r2)
 		elif op1 == 5:
 			backend.output_op(val, r1, r2)
 		elif op1 == 6:
 			backend.phi_op(val, r1, r2)
 		else:
-			raise Exception("Invalide opcode1 %d" % op1)
+			raise Exception("Invalid opcode1 %d" % op1)
