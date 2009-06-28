2f5ec7b25be63ea8cdd01c1b682f94a05f2ab658
Tobias Deutsch ICFP <toby@ti11.(none)>
2009-06-28 20:17:37 +0200

    compile clean simulation

--- simulator/simulation.ml
+++ simulator/simulation.ml
@@ -9,27 +9,34 @@ let config_to_problem config_id =
   | 3001 | 3002 | 3003 | 3004 -> Vm.Eccentric
   | 4001 | 4002 | 4003 | 4004 -> Vm.ClearSky
   | _ -> failwith "unkown configuration id";;
+(*
+let apply_frame frame =
+  ();;
 
+let step t m frames =
+  match frames with 
+      (timestamp,frame)::tail->if t=timestamp then
+    begin
+      apply_frame frame;
+      tail
+    end
+  else
+    frames
+    | _ -> frames;;
 
-
-let x = 1;;
-
-let step m
 
 let run_simulation osf_filename =
   let (config_id, frames_t) = read_osf_file osf_filename in
   let frames = ref frames_t in
   let problem_type = config_to_problem config_id  in
   let m = vm_init_machine problem_type in
-  let t = 0 in 
-  let rec loop m t =
-    step t m frames;
-    if !vm_is_done then
-      loop (step m frames) (t+1)
-    else 
-      ()
-  in 
-    loop m t;
-    ;;
+  let t = ref 0 in 
+  let output = ref [] in
+  while not (vm_is_done m) do
+    let o = step t m frames in
+      output := o :: !output;
+    t := !t + 1;
+  done;
+   ();;
 
-
+*)
